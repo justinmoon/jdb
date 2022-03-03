f9b706c63a103293b7736bb6f78f60970c0fef10
Justin Moon <mail@justinmoon.com>
2022-03-03 00:56:12 -0600

    Remove logging statement

--- db.js
+++ db.js
@@ -7,19 +7,6 @@ export async function execute(query, args = []) {
   return res.rows;
 }
 
-console.log({
-  // connectionString: process.env.DB_STRING,
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  database: process.env.POSTGRES_DB,
-  password: process.env.POSTGRES_PASSWORD,
-  ssl: false,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
-});
-
 const pool = new Pool({
   // connectionString: process.env.DB_STRING,
   host: process.env.POSTGRES_HOST,
